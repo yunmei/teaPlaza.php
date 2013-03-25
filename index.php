c1dee63212ebbbe440d7c628c2ed551ef1336444
Bevin <bevin1984@gmail.com>
2013-03-25 13:39:45 +0800

    Move public/ to /

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('PRC');
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
-$config=dirname(__FILE__).'/../protected/config/main.php';
-$global = dirname(__FILE__) . '/../yii/global.php';
+$yii=dirname(__FILE__).'/yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+$global = dirname(__FILE__) . '/yii/global.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
