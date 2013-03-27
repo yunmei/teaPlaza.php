4d10a2cbbddeb762ef99ea49afc992658a1479d9
Bevin <bevin1984@gmail.com>
2013-03-27 09:55:45 +0800

    Add app.getAppContent, app.getListInfo

--- protected/modules/api/models/Api_App.php
+++ protected/modules/api/models/Api_App.php
@@ -29,13 +29,65 @@ class Api_App
 	 */
 	public function getAppList($args, $format)
 	{
+		$num = intval($_GET['num']);
+		$num = $num ? $num : 8;
 		$array = array(
-			array('id'=>'1', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/1.png'),
-			array('id'=>'2', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/2.png'),
-			array('id'=>'3', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/3.png'),
-			array('id'=>'4', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/4.png'),
-			array('id'=>'5', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/5.png')
+			array('id'=>'1', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/1.png', 'name'=>'开心茶园'),
+			array('id'=>'2', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/2.png', 'name'=>'买买茶'),
+			array('id'=>'3', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/6.png', 'name'=>'茶百科'),
+			array('id'=>'4', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/4.png', 'name'=>'单店ERP'),
+			array('id'=>'5', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/5.png', 'name'=>'御青茗茶'),
+			array('id'=>'6', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/3.png', 'name'=>'我来泡茶'),
+			array('id'=>'1', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/1.png', 'name'=>'开心茶园'),
+			array('id'=>'2', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/2.png', 'name'=>'买买茶'),
+			array('id'=>'3', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/6.png', 'name'=>'茶百科'),
+			array('id'=>'4', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/4.png', 'name'=>'单店ERP'),
+			array('id'=>'5', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/5.png', 'name'=>'御青茗茶'),
+			array('id'=>'6', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/3.png', 'name'=>'我来泡茶'),
 		);
+		$array = array_slice($array, 0, $num);
+		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>$array);
+		return $data;
+	}
+
+	/**
+	 * 获取列表详情
+	 * version 1.0
+	 */
+	public function getListInfo($args, $format)
+	{
+		$num = intval($_GET['num']);
+		$num = $num ? $num : 8;
+		$array = array(
+			array('id'=>'1', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/1.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '3213', 'name'=>'开心茶园'),
+			array('id'=>'2', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/2.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '789', 'name'=>'买买茶'),
+			array('id'=>'3', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/3.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '6554', 'name'=>'茶百科'),
+			array('id'=>'4', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/3.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '11257', 'name'=>'单店ERP'),
+		);
+		$array = array_slice($array, 0, $num);
+		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>$array);
+		return $data;
+	}
+
+	/**
+	 * 获取应用详情
+	 * version 1.0
+	 */
+	public function getAppContent($args, $format)
+	{
+		$array = array(
+			'id' => '1',
+			'name' => '开心茶园',
+			'icon' => 'http://teaplaza.maimaicha.com/upload/icon/1.png',
+			'listimage' => 'http://teaplaza.maimaicha.com/upload/ad/1.jpg',
+			'desc' => '很好玩的游戏，欢迎来玩！',
+			'urlschemes' => 'com.maimaicha.chabaike://haha',
+			'ituneslink' => 'https://itunes.apple.com/cn/app/cha-bai-ke/id527790886?mt=8',
+			'html' => '<html><header></header><body style="margin:0px; padding:0px; background-color:#00ffff;"><div style="font-size:24px; color:red;"><strong>开心茶园</strong><br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园</div></body></html>',
+			'version' => '1.0',
+			'usenum' => '218'
+		);
+		$array = array_slice($array, 0, $num);
 		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>$array);
 		return $data;
 	}
