676689445dc03ac14680e5723baf3b0d560d6bb9
Bevin <bevin1984@gmail.com>
2013-03-28 17:59:52 +0800

    Fetch Data from DB

--- protected/modules/api/models/Api_App.php
+++ protected/modules/api/models/Api_App.php
@@ -15,9 +15,10 @@ class Api_App
 	public function getAdList($args, $format)
 	{
 		$array = array(
-			array('id'=>'1', 'image'=>'http://teaplaza.maimaicha.com/upload/ad/1.jpg'),
-			array('id'=>'2', 'image'=>'http://teaplaza.maimaicha.com/upload/ad/2.jpg'),
-			array('id'=>'3', 'image'=>'http://teaplaza.maimaicha.com/upload/ad/3.jpg')
+			array('id'=>'1', 'image'=>'http://teaplaza.maimaicha.com/upload/ad/1.png'),
+			array('id'=>'2', 'image'=>'http://teaplaza.maimaicha.com/upload/ad/2.png'),
+			array('id'=>'3', 'image'=>'http://teaplaza.maimaicha.com/upload/ad/3.png'),
+			array('id'=>'4', 'image'=>'http://teaplaza.maimaicha.com/upload/ad/4.png')
 		);
 		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>$array);
 		return $data;
@@ -31,21 +32,16 @@ class Api_App
 	{
 		$num = intval($_GET['num']);
 		$num = $num ? $num : 8;
-		$array = array(
-			array('id'=>'1', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/1.png', 'name'=>'开心茶园'),
-			array('id'=>'2', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/2.png', 'name'=>'买买茶'),
-			array('id'=>'3', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/6.png', 'name'=>'茶百科'),
-			array('id'=>'4', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/4.png', 'name'=>'单店ERP'),
-			array('id'=>'5', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/5.png', 'name'=>'御青茗茶'),
-			array('id'=>'6', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/3.png', 'name'=>'我来泡茶'),
-			array('id'=>'1', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/1.png', 'name'=>'开心茶园'),
-			array('id'=>'2', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/2.png', 'name'=>'买买茶'),
-			array('id'=>'3', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/6.png', 'name'=>'茶百科'),
-			array('id'=>'4', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/4.png', 'name'=>'单店ERP'),
-			array('id'=>'5', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/5.png', 'name'=>'御青茗茶'),
-			array('id'=>'6', 'icon'=>'http://teaplaza.maimaicha.com/upload/icon/3.png', 'name'=>'我来泡茶'),
-		);
-		$array = array_slice($array, 0, $num);
+		$criteria = new CDbCriteria();
+		$criteria->select = "id, icon, name";
+		$criteria->addColumnCondition(array('state'=>1));
+		$criteria->order = 'usenum desc';
+		$criteria->limit = $num;
+		$rowset = App::model()->findAll($criteria);
+		$attributes = array('id', 'icon', 'name');
+		foreach ($rowset as $n) {
+			$array[] = ApiBase::object2array($n, $attributes);
+		}
 		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>$array);
 		return $data;
 	}
@@ -58,13 +54,16 @@ class Api_App
 	{
 		$num = intval($_GET['num']);
 		$num = $num ? $num : 8;
-		$array = array(
-			array('id'=>'1', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/1.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '3213', 'name'=>'开心茶园'),
-			array('id'=>'2', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/2.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '789', 'name'=>'买买茶'),
-			array('id'=>'3', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/3.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '6554', 'name'=>'茶百科'),
-			array('id'=>'4', 'listimage'=>'http://teaplaza.maimaicha.com/upload/ad/3.jpg', 'desc' => '很好玩的游戏，欢迎来玩！', 'usenum' => '11257', 'name'=>'单店ERP'),
-		);
-		$array = array_slice($array, 0, $num);
+		$criteria = new CDbCriteria();
+		$criteria->select = "id, listimage, `desc`, usenum, name";
+		$criteria->addColumnCondition(array('state'=>1));
+		$criteria->order = 'usenum desc';
+		$criteria->limit = $num;
+		$rowset = App::model()->findAll($criteria);
+		$attributes = array('id', 'listimage', 'desc', 'usenum', 'name');
+		foreach ($rowset as $n) {
+			$array[] = ApiBase::object2array($n, $attributes);
+		}
 		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>$array);
 		return $data;
 	}
@@ -75,20 +74,12 @@ class Api_App
 	 */
 	public function getAppContent($args, $format)
 	{
-		$array = array(
-			'id' => '1',
-			'name' => '开心茶园',
-			'icon' => 'http://teaplaza.maimaicha.com/upload/icon/1.png',
-			'listimage' => 'http://teaplaza.maimaicha.com/upload/ad/1.jpg',
-			'desc' => '很好玩的游戏，欢迎来玩！',
-			'urlschemes' => 'com.maimaicha.chabaike://haha',
-			'ituneslink' => 'https://itunes.apple.com/cn/app/cha-bai-ke/id527790886?mt=8',
-			'html' => '<html><header></header><body style="margin:0px; padding:0px; background-color:#00ffff;"><div style="font-size:24px; color:red;"><strong>开心茶园</strong><br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园<br />开心茶园</div></body></html>',
-			'version' => '1.0',
-			'usenum' => '218'
-		);
-		$array = array_slice($array, 0, $num);
-		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>$array);
+		$id = intval($_GET['id']);
+		$criteria = new CDbCriteria();
+		$criteria->addColumnCondition(array('state'=>1, 'id'=>$id));
+		$row = App::model()->find($criteria);
+		$attributes = array('id', 'name', 'urlschemes', 'ituneslink', 'html');
+		$data = array('errorCode' => ApiError::SUCCESS, 'errorMessage'=>'', 'result'=>ApiBase::object2array($row, $attributes));
 		return $data;
 	}
 	
@@ -117,6 +108,7 @@ class Api_App
 	{
 		$content = trim($_GET['content']);
 		$contact = trim($_GET['contact']);
+		$version = trim($_GET['version']);
 		if(1) {
 			return ApiBase::$success;
 		} else {
