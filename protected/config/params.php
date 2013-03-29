28fed59e9b87bf77b6444a07d94a594c364c639d
Bevin <bevin1984@gmail.com>
2013-03-29 15:09:07 +0800

    Add RequestDataUrl

--- protected/config/params.php
+++ protected/config/params.php
@@ -17,4 +17,6 @@ return array(
     'priorityRegister' => 1000,
     'priorityForgetPasswd' => 1000,
 
+	// 请求积分买币
+	'RequestDataUrl' => 'http://www.ms2b.com/api?apikey=b4f4ee31a8b9acc866ef2afb754c33e6&format=json&method=game.getUserScPoDi&ucid=',
 );
